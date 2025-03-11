43eb97abd3f8a79a5bfdcfed49ba6825ddb9b3ad
Duds <meduardarodrigues91@gmail.com>
2025-03-11 11:36:08 -0300

    criando o codigo
    em andamento

---
+++ codigo.py
@@ -0,0 +1,61 @@
+import pandas as pd #vai ler o arquivo excele  evai armazena-lo
+
+
+#importar base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+
+#visualização da base de dados
+pd.set_option('display.max_columns', None)#mostrar as colunas sem um limite máximo definido
+print(tabela_vendas)
+
+
+#faturamento (valor final)
+faturamento = tabela_vendas [['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+
+#quantidade de produtos
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
+
+print('-' * 50)
+#ticket medio por produto
+ticket_medio = (faturamento['Valor Final'] / quantidade ['Quantidade']).to_frame()
+
+
+
+
+#enviar gmail com relatório
+import win32com.client as win32
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreatItem(0)
+mail.To = 'meduardarodrigues@gmail.com'
+mail.Subject = 'Relatório de Vendas'
+mail.HTMLBody= '''
+Prezadas,
+Segue o Relatório de vendas por cada loja!
+
+
+Faturamento: {}
+
+
+Quantidade Vendida: {}
+
+
+Ticket Médio dos Produtos em cada Loja: {}
+
+
+Qualquer duvida estarei á disposição!!
+Att.,
+Maria
+'''
+
+
+mail.Send()
+
+
+print('Email Enviado!')
+
+
